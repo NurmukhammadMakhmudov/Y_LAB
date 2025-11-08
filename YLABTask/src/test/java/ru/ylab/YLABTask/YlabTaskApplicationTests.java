61d094013d8b7050a2b5bbb4ba477eeca13cba71
nurmukhammadmakhmudov <Fiqxu8-qyqtap-wakjuk>
2025-11-08 14:25:35 +0300

    initialization of spring project

---
+++ YLABTask/src/test/java/ru/ylab/YLABTask/YlabTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ylab.YLABTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlabTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
